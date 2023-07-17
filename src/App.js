43ac25fae5ec9fa96055a1faa48531254d3e1e0b
rupeshgondrawar <rupeshgondrawar462@gmail.com>
2023-07-17 12:24:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import {useRef} from "react"
 
 function App() {
+
+  const ref = useRef(null);
+
+  const onPlay=()=>{
+    ref.current.play();
+  }
+  const onPause=()=>{
+    ref.current.pause();
+  }
+  console.log(Date.now())
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>video assignment</h1>
+      <video ref={ref} controls width="250">
+        <source
+          src="https://www.w3schools.com/html/movie.wbm"
+          type="video/webm"
+        />
+
+        <source
+          src="https://www.w3schools.com/html/movie.mp4"
+          type="video/mp4"
+        />
+      </video>
+
+      <div>
+        <button onClick={onPlay}>play</button>
+        <button onClick={onPause}>pause</button>
+        <button>skip</button>
+      </div>
     </div>
+    
   );
 }
 
